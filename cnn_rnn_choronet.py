e99aa1c19257a55a9d11d0bfa59fa8485ff63f31
Khushi310105 <khushi310105@gmail.com>
2026-05-20 15:36:44 +0530

    cnn-gru

--- cnn_rnn_choronet.py
+++ cnn_rnn_choronet.py
@@ -32,3 +32,35 @@ out2.shape
 block= Block(in_channels=out2.shape[1])
 out3=block(out2)
 out3.shape
+
+gru= nn.GRU(input_size=96,hidden_size=32,batch_first=True)
+x=out3.permute(0,2,1)
+output,hn= gru(x)
+output.shape
+
+gru1= nn.GRU(input_size=96,hidden_size=32,batch_first=True)
+gru2= nn.GRU(input_size=32,hidden_size=32,batch_first=True)
+gru_out1, _ = gru1(x)
+gru_out2, _ = gru2(gru_out1)
+gru_out=torch.cat((gru_out1,gru_out2),dim=2)
+gru_out.shape
+# gru_out1.shape
+
+gru3= nn.GRU(input_size=64,hidden_size=32,batch_first=True)
+gru_out3, _ = gru3(gru_out)
+gru_out3.shape
+
+gru_out=torch.cat((gru_out1,gru_out2,gru_out3),dim=2)
+gru_out.shape
+
+gru4= nn.GRU(input_size=96,hidden_size=32,batch_first=True)
+gru_out4, _ = gru4(gru_out)
+gru_out4.shape
+
+linear= nn.Linear(in_features=1875, out_features=1)
+linear_out= linear(gru_out.permute(0,2,1))
+linear_out.shape
+
+gru4= nn.GRU(input_size=96,hidden_size=32,batch_first=True)
+gru_out4, _ = gru4(linear_out.permute(0,2,1))
+gru_out4.shape
